2aed2f42f2dfc27e55eda87c5d30ff0b2c1dce84
fhelry <fhelrypongase@yahoo.com>
2024-09-23 20:13:20 +0800

    structured signup screen

--- lib/screens/signUp.dart
+++ lib/screens/signUp.dart
@@ -6,15 +6,38 @@ class signup extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: LayoutBuilder(
-        builder: (context, constraints){
-          return const Column(
-            children: [
-              Text("Test")
-            ],
-          );
-        }
-        ),
+      body: LayoutBuilder(builder: (context, constraints) {
+        return Center(
+          child: Card(
+            elevation: 10,
+            child: Container(
+              width: constraints.maxWidth * 0.7,
+              height: constraints.maxHeight * 0.6,
+              child: const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                Text("First name"),
+                Text("Last name"),
+                Text("Email"),
+                Text("Password (6 or more characters)"),
+
+                Text("By clicking, you agree to our Terms of Service & Privacy Policy"),
+
+                //! Buttons and Divider
+                Column(
+                  children: [
+                    Text("Join now"),
+                    Text("2 logos"),
+
+                    Text("Already on the app? Sign in")
+                  ],
+                )
+                ]
+              ),
+            ),
+          ),
+        );
+      }),
     );
   }
 }
